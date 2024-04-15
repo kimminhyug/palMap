777e84539c4fa730dcbe2aee9d50c73fa5cfa883
unknown <kimminhyug@naver.com>
2024-04-15 21:11:02 +0900

    add boxshadown(multl elmental)

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -31,6 +31,18 @@ const JOB_IMAGES = {
   WATERING: 'watering.pnG',
 };
 const imageSize = 48;
+const ELEMENTAL: { [key: string]: string } = {
+  WATER: '#7bd3ea',
+  FIRE: '#ff4545',
+  DARK: 'rgb(156, 16, 74)',
+  GROUND: 'rgb(143, 85, 35)',
+  ICE: 'rgb(24, 176, 192)',
+  NEUTRAL: 'rgb(181, 148, 140)',
+  ELECTRIC: 'rgb(206, 165, 0)',
+  GRASS: 'rgb(56, 240, 79)',
+  DRAGON: 'YELLOW',
+};
+
 export const Card = ({ pal }: { pal: IPalInfo }): React.ReactElement => {
   const [flipStatus, setFlipStatus] = useState(false);
 
@@ -38,9 +50,25 @@ export const Card = ({ pal }: { pal: IPalInfo }): React.ReactElement => {
     // console.log(flipStatus);
     // setFlipStatus((prev) => !prev);
   };
+  const elementalLength = pal.types.length;
+  const boxShadowCSS = pal.types
+    .map((type, idx) => {
+      if (!ELEMENTAL[type.toUpperCase()]) console.log([type.toUpperCase()]);
+      return `${(30 / elementalLength) * (idx + 1)}px ${30 / elementalLength}px ${5 / elementalLength}px ${1}px ${
+        ELEMENTAL[type.toUpperCase()]
+      }`;
+    })
+    .join();
+
   return (
     <>
-      <div className={`card ${pal.types[0]}`} onClick={toggleFlip}>
+      <div
+        className={`card ${pal.types[0]}`}
+        onClick={toggleFlip}
+        style={{
+          boxShadow: boxShadowCSS,
+        }}
+      >
         <div className={`card-container  ${flipStatus ? 'disable' : ''}`}>
           <div className="card-header d-f">
             <div className="card-element">
@@ -71,7 +99,7 @@ export const Card = ({ pal }: { pal: IPalInfo }): React.ReactElement => {
             </div>
           </div>
           {/* back */}
-          {/* <div className={`info ${flipStatus ? 'show' : ''}`}>
+          {/* <div className={`info ${flipStatus ? 'show': ''}`}>
             <div className="card-b">i am back</div>
           </div> */}
         </div>
